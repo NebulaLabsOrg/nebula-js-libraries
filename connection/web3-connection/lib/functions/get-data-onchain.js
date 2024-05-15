718abb9619685699ae1581c88c12e31843ba4580
Gloom0x0 <gloomneon@gmail.com>
2024-05-15 13:46:54 +0200

    Added limit order library

--- connection/web3-connection/lib/functions/get-data-onchain.js
+++
@@ -1,9 +0,0 @@
-import ethers from ethers
-
-export async function getNetworkDataConnected(_provider){
-    return await _provider.getNetwork()
-}
-
-export async function getNetworkData(_chainId){
-    return await ethers.providers.getNetwork(_chainId)
-}
